249e4db76446083276577e03e835ec3e715182c1
adnanj <adnan.webdev@gmail.com>
2020-02-18 08:51:30 +1100

    stuck in the fuckery of scoping and closures

--- 05/junk.js
+++ 05/junk.js
@@ -28,3 +28,9 @@ testCallback ('my cup', cBack);
 
   testing('cup',logQuote);
  */
+
+ for (var i = 1; i <= 5; i++) {
+   setTimeout(function() {
+     console.log("i: " + i);
+   }, i * 1000);
+ }
