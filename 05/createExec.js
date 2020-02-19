6e497e58bfbb505ffe47df4f11d3fee5fb4c1be5
FABolous <faheema@cos.net.au>
2020-02-19 12:06:30 +1100

    dont know

--- 05/createExec.js
+++ 05/createExec.js
@@ -1,5 +1,21 @@
 for (var i = 1; i <= 5; i++) {
+
   setTimeout(function() {
+
     console.log("i: " + i);
+
   }, i * 1000);
+
 }
+
+// var input = document.getElementById("new");
+
+// input.onclick = function() {
+//   setTimeout(function() {
+//     input.value +=' input'
+//   }, 0)
+// }
+
+// document.body.onclick = function() {
+//   input.value += ' body'
+// }
